634f661e9f46a12692a543478b8402ea5749f2be
Jennifer <jennifermorgan2001@gmail.com>
2020-04-06 16:24:39 +0000

    Origin

---
+++ lib/cards.ex
@@ -0,0 +1,47 @@
+defmodule Cards do
+  @moduledoc """
+    Provides method for creating and handling a deck of cards
+  """
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck,card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck,hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename)  do
+    case File.read(filename) do
+       {:ok, file_data} -> :erlang.binary_to_term(file_data)
+       _ -> "Could not find file"
+    end
+  end
+
+  def create_hand(hand_size) do
+    # deck = Cards.create_deck
+    # deck = Cards.shuffle(deck)
+    # hand = Cards.deal(deck, hand_size)
+    Cards.create_deck 
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
+end
